2c75fa57bcfca8c979f6b791e1b4bb1ebf77a7e1
Hassan Alizadeh <hasan.alizadeh@arvancloud.com>
2022-10-26 10:04:38 +0330

    fix: initial state of theme

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -47,6 +47,7 @@ export default {
       theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
     }
     document.documentElement.setAttribute('data-theme', theme)
+    this.theme = theme
   }
 }
 </script>
